83eb062a4143a78d019934334f49fa11920da853
Fitra Aditya <fitra@g.pl>
2015-09-02 14:02:55 +0700

    Format json object

--- app/models/session.js
+++ app/models/session.js
@@ -1,9 +1,17 @@
 var Mongoose = require('../../database').Mongoose
 
 var sessionSchema = new Mongoose.Schema({
-  session: { type : String, required: true },
-  token: { type : String, required: true },
-  addedDate: { type : Date, default: Date.now }
+  session: {
+    type : String,
+    required: true
+  },
+  token: {
+    type : String, required: true
+  },
+  addedDate: {
+    type : Date,
+    default: Date.now
+  }
 })
 
 exports.Session = Mongoose.model('Session', sessionSchema)
